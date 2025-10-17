394c19884c4a40cd6b58d12cbc5b622651944ae3
Qonda <138157862+Qonda3@users.noreply.github.com>
2025-10-17 10:46:49 +0200

    Refactor Deck class to initialize cards using shared color, number, action, and wild logic

--- game_logic.py
+++ game_logic.py
@@ -11,3 +11,17 @@ class Card:
         self.value = value
     def __str__(self):
         return f"{self.color} {self.value}" if self.color else self.value
+    
+class Deck:
+    def __init__(self):
+        self.cards = []
+        for color in COLORS:
+            for n in NUMBERS:
+                self.cards.append(Card(color, n))
+                if n != 0:
+                    self.cards.append(Card(color, n))
+            for a in ACTIONS:
+                self.cards.extend([Card(color, a), Card(color, a)])
+        for w in WILDS:
+            self.cards.extend([Card(None, w) for _ in range(4)])
+        random.shuffle(self.cards)
